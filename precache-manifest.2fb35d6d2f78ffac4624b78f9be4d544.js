b2ba0b8695e72eb080b2be7dd4df0d02069879b0
nick <nicholasdipreta2020@gmail.com>
2020-06-24 09:44:53 -0400

    Updates

---
+++ precache-manifest.2fb35d6d2f78ffac4624b78f9be4d544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c74f02c1b6165b01dafa14cdf816f78",
+    "url": "/istock/index.html"
+  },
+  {
+    "revision": "8ecc79602634e1cc7be0",
+    "url": "/istock/static/css/main.288d6c66.chunk.css"
+  },
+  {
+    "revision": "beadcbb4aff305dd96de",
+    "url": "/istock/static/js/2.c7361d41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/istock/static/js/2.c7361d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecc79602634e1cc7be0",
+    "url": "/istock/static/js/main.09f22ebb.chunk.js"
+  },
+  {
+    "revision": "db8598939a2e7a72d83d",
+    "url": "/istock/static/js/runtime-main.2e9ca206.js"
+  }
+]);
